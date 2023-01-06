71a3bf407ffd0d64d9b8371efb202ebf4197ca53
KesterDaniel <kesterdan17@gmail.com>
2023-01-06 09:57:36 +0100

    defined operation functions

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
 # Python calculator
+
+# Add
+def add(n1, n2):
+  """Adds two numbers n1 and n2"""
+  return n1 + n2
+
+# Subtract
+def subtract(n1, n2):
+  """Subtracts n2 from n1"""
+  return n1 - n2
+
+# Multiply
+def multiply(n1, n2):
+  """Multiply n1 by n2"""
+  return n1 * n2
+
+# Divide
+def divide(n1, n2):
+  """Divide n1 by n2"""
+  return n1 / n2
